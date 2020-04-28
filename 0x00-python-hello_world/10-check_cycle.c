ef87c6ad232509be5394bc2bb8ae4a13f700caf6
s-macias <1552@holbertonschool.com>
2020-04-28 04:34:12 +0000

    Task 10 draft

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks a cycle
+ * @list: list
+ *
+ * Returns: 0 if there's a cycle, 1 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	int n = 0;
+
+	if (list)
+		n = 1;
+	return (n);
+}
