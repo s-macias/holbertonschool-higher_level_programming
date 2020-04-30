fad97b3a15e6c8e14c87b2bcb2acccbb3672bbc2
s-macias <1552@holbertonschool.com>
2020-04-30 17:26:36 +0000

    fixed indentation errors

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,9 +3,11 @@ import dis
 
 
 def magic_calculation(a, b, c):
-        if a > b:
-            return c
-        if c > b:
-            return a + b
-        return a * b - c
+    if a > b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
+
+
 dis.dis(magic_calculation)
