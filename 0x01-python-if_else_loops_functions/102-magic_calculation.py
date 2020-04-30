f14af0d10c181efdf0eab647098943972914afe2
s-macias <1552@holbertonschool.com>
2020-04-30 16:22:27 +0000

    Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+import dis
+
+
 def magic_calculation(a, b, c):
-    dis.dis(magic_calculation)
+        if a > b:
+            return c
+        if c > b:
+            return a + b
+        return a * b - c
+dis.dis(magic_calculation)
