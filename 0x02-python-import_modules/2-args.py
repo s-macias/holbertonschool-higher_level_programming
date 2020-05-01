5d20dc6cbd1f98b2507f58b8a2589cd546d8bfe6
s-macias <1552@holbertonschool.com>
2020-05-01 04:43:26 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+    if ac == 0:
+        print("{:d} arguments.".format(ac))
+    elif ac == 2:
+        print("{:d} argument:".format(ac))
+    elif ac > 2:
+        print("{:d} arguments:".format(ac))
+    for i in range(1, ac):
+        print("{:d}: {:s}".format(i, arg[i]))
