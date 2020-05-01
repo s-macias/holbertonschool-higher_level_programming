1d4e73b813b89f742df8a431b757369f65263593
s-macias <1552@holbertonschool.com>
2020-05-01 04:51:44 +0000

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+    sum = 0
+    for i in range(1, ac):
+        sum += int(argv[i])
+
+    print("{:d}".format(sum))
