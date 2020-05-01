4854fd6aeb9e9d374cf55339fb4d7c22fd1a46df
s-macias <1552@holbertonschool.com>
2020-05-01 03:38:05 +0000

    trying different if condition

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,14 +2,10 @@
 from calculator_1 import add, sub, mul, div
 
 
-def main():
+if __name__ == "_main__":
     a = 1
     b = 5
     print("{:d} +  {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} -  {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} *  {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} /  {:d} = {:d}".format(a, b, div(a, b)))
-
-
-if __name__ == "_main__":
-    main()
