aa75297c51e8e9ce8c3000a50bbaf5bf79fca293
s-macias <1552@holbertonschool.com>
2020-05-06 03:41:36 +0000

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        new_list = my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     else:
         my_list[idx] = element
     return my_list
