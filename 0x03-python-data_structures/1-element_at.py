fe94abe058d23825197fab8c0c34f714161f3d56
s-macias <1552@holbertonschool.com>
2020-05-06 03:36:58 +0000

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-
-    if idx < 0:
-        return None
-    elif len(my_list) == 0:
-        return None
-    elif idx > len(my_list):
+    if idx < 0 or len(my_list) == 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
