c298690b473837e18e556d1c4f58f2b5af79aed3
s-macias <1552@holbertonschool.com>
2020-05-08 04:58:01 +0000

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
+    new_dic = a_dictionary.copy()
     return new_dic
