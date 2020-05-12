8890251f7d82c5929cd16eb91451fedfdf11530c
s-macias <1552@holbertonschool.com>
2020-05-12 03:51:49 +0000

    task 2 fixed some cases

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    ac = len(argv) - 1
+    import sys
+    ac = len(sys.argv) - 1
     if ac == 0:
         print("{:d} arguments.".format(ac))
-    elif ac == 2:
-        print("{:d} argument:".format(ac))
-    elif ac > 2:
+    elif ac == 1:
+        print("{:d} argument:\n{:d}: {:s}".format(ac, ac, sys.argv[1]))
+    else:
         print("{:d} arguments:".format(ac))
-    for i in range(1, ac):
-        print("{:d}: {:s}".format(i, arg[i]))
+    for i in range(0, len(sys.argv)):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
