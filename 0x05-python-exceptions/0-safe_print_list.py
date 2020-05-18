83e157d00612aff90506e68fce2aad6754376d32
s-macias <1552@holbertonschool.com>
2020-05-18 22:03:33 +0000

    TAsk 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    number_items = 0
+    try:
+        for i in range(x):
+            print("¨{:d}".format(my_list[i]), end="")
+            number_items += 1
+        print()
+    except:
+        print()
+    return number_items
