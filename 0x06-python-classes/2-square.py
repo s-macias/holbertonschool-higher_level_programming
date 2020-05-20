01269e9ae75414380e30922b10cd7797874d62d5
s-macias <1552@holbertonschool.com>
2020-05-20 04:27:51 +0000

    Task 2 fixing error handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,10 @@ class Square:
     """size must be an integer (TypeError) and greater than 0 (ValueError)"""
 
     def __init__(self, size=0):
-        try:
+        """optional initialization with size, verify value and type"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self._size = size
-        except (TypeError):
-            print("size must be an integer")
-        except (ValueError):
-            print("size must be >= 0")
