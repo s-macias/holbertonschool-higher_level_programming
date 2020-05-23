02549c5469d5882b154ee917b6d0242dcb28cce5
s-macias <1552@holbertonschool.com>
2020-05-23 20:57:15 +0000

    fix test task 0

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """class to perform unittests
+    """    
+    
+    def test_empty_list(self):
+        self.assertEqual(a, b)
+
+    def test_one_item(self):
+        self.assertTrue()
+        self.assertFalse()
+        
+    def test_positive_integers(self):
+    
+    def test_positive_negative(self):
+    
+    def test_floats(self):
+    
+    def test_type(self):
+    
+    def test_negative(self):
+    
+    def test_zero_positive(self):
+    
+    def test_zero_negative(self):
+    
+            
+
+    
