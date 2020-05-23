0c256ce90167402bbe473dfae5c1eafa7ae5f829
s-macias <1552@holbertonschool.com>
2020-05-23 19:03:07 +0000

    task 4 fixed pep8

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -24,4 +24,4 @@ def print_square(size):
     elif isinstance(size, int) and size < 0:
         raise ValueError("size must be >= 0")
     for i in range(0, size):
-         print("#" * size)
+        print("#" * size)
