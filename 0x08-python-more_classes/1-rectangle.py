6c64fc9dee4f2b2dd3e933021eeff91ebfe60e35
s-macias <1552@holbertonschool.com>
2020-05-27 16:21:58 +0000

    task 1 fixed indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,61 +19,57 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
+    def width(self):
+        """
+        gets width
 
-@property
-def width(self):
-    """
-    gets width
+        Returns:
+            width (int)
+        """
+        return self.__width
 
-    Returns:
-        width (int)
-    """
-    return self.__width
+    @width.setter
+    def width(self, value):
+        """
+        sets width of the rectangle
 
+        Attributes:
+            width (int)
 
-@width.setter
-def width(self, value):
-    """
-    sets width of the rectangle
+        Raises:
+            TypeError: width is not an integer
+            ValueError: width is negative
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-    Attributes:
-        width (int)
+    @property
+    def height(self):
+        """
+        gets height
+        Returns:
+            height (int)
+        """
+        return self.__height
 
-    Raises:
-        TypeError: width is not an integer
-        ValueError: width is negative
-    """
-    if type(value) is not int:
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
+    @height.setter
+    def height(self, value):
+        """
+        sets height of the rectangle
 
+        Attributes:
+            height (int)
 
-@property
-def height(self):
-    """
-    gets height
-    Returns:
-        height (int)
-    """
-    return self.__height
-
-
-@height.setter
-def height(self, value):
-    """
-    sets height of the rectangle
-
-    Attributes:
-        height (int)
-
-    Raises:
-        TypeError: height is not an integer
-        ValueError: height is negative
-    """
-    if type(value) is not int:
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = value
+        Raises:
+            TypeError: height is not an integer
+            ValueError: height is negative
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
