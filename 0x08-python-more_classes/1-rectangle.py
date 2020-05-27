33729b88ba4d2a214d14c331a20880cc8cacea7c
s-macias <1552@holbertonschool.com>
2020-05-27 15:53:39 +0000

    changed if statements

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -45,10 +45,9 @@ def width(self, value):
     """
     if type(value) is not int:
         raise TypeError("width must be an integer")
-    elif value < 0:
+    if value < 0:
         raise ValueError("width must be >= 0")
-    else:
-        self.__width = value
+    self.__width = value
 
 
 @property
@@ -75,7 +74,6 @@ def height(self, value):
     """
     if type(value) is not int:
         raise TypeError("height must be an integer")
-    elif value < 0:
+    if value < 0:
         raise ValueError("height must be >= 0")
-    else:
-        self.__height = value
+    self.__height = value
