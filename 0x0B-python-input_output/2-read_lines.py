e5f96a1ec707c8f8ab16d89dc07c698b4c3379e5
s-macias <1552@holbertonschool.com>
2020-06-04 03:23:24 +0000

    Task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""reads n lines of a text file (UTF8) and prints it to stdout"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """reads n lines of a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: name of the file
+        nb_lines: number of lines
+    """
+    with open(filename, encoding="utf-8") as myFile:
+        f_len = myFile.readlines()
+    if nb_lines <= 0 | nb_lines >= len(f_len):
+        print(file.readlines(), end="")
+    else:
+        print(myFile.readline(), end="")
