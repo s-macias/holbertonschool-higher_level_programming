022fcb1e6dd87b117a327c446bb1faea7f99e7cd
s-macias <1552@holbertonschool.com>
2020-06-10 05:13:02 +0000

    test-base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,12 +5,8 @@
 import unittest
 import pep8
 from models.base import Base
-from models.rectangle import rectangle
-from models.square import Square
 
-"""
-    Tests for base class
-"""
+t
 
 
 class TestBase(unittest.TestCase):
@@ -20,8 +16,4 @@ class TestBase(unittest.TestCase):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['./models/base.py])
-        self.assertEqual(result.total_errors, 0)
-
-
-
-
+        self.assertEqual(result.total_errors, 0, "Found code style erros (and warnings).")
