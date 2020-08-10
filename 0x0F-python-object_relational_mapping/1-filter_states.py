ee348244b9ac77f23b9e79f0ce43feac94e8509e
s-macias <1552@holbertonschool.com>
2020-08-10 00:35:19 +0000

    Task 1 removed print cur

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    print(cur)
     rows = cur.fetchall()
     for row in rows:
         print(row)
