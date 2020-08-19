eb783420c9b0ce24a97132160d6a4552f71ba8c6
s-macias <1552@holbertonschool.com>
2020-08-19 00:27:38 +0000

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
-curl -w '%{size_download}\n' -s -o /dev/null "$1"
+curl -w '%{size_download}\n' -so /dev/null "$1"
