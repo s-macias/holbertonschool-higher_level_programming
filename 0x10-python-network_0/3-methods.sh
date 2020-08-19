1c6d72b8a9e267ed9b661d1f152aeae9a2413eed
s-macias <1552@holbertonschool.com>
2020-08-19 02:47:15 +0000

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+# Bash script that sends a DELETE request to the URL passed as the first
+# argument and displays the body of the response
+curl -sI "$1" | grep Allow | cut -d ' ' '-f2-'
