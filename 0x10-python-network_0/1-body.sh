40f167b5f1bb34c057e9c0aa2a12d4afc439591e
s-macias <1552@holbertonschool.com>
2020-08-24 13:26:13 +0000

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays
-# the body of the response
+# Bash script that displays the body of the response for a GET request
 curl -sLX GET"$1"
