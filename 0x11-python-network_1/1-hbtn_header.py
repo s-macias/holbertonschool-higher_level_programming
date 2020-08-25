6177a37cb847434d83f268bffd9d831a3804eebf
s-macias <1552@holbertonschool.com>
2020-08-25 01:46:45 +0000

    task 1 added if__main__

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.info()['X-request-Id']
-    print(x_request_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.info()['X-request-Id']
+        print(x_request_id)
