68a8e5b4dea6fc181d7fdff8d8eb484a75fb1e15
s-macias <1552@holbertonschool.com>
2020-08-25 02:46:58 +0000

    task 0 adding if __main__

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: <class 'bytes'>")
-    print("\t- content: b'OK'")
-    print("\t- utf8 content: OK")
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: <class 'bytes'>")
+        print("\t- content: b'OK'")
+        print("\t- utf8 content: OK")
