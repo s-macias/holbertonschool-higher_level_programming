530ae3f1172888f0ddfd4271f233278583ecf508
s-macias <1552@holbertonschool.com>
2020-08-25 02:21:11 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.info()['X-request-Id']
+        x_request_id = response.info()['X-Request-Id']
         print(x_request_id)
