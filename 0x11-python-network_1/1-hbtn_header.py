6661eb6ce7fc04526f1af8189d5eff648f794208
s-macias <1552@holbertonschool.com>
2020-08-25 02:50:41 +0000

    removing if...main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,7 @@ import urllib.request
 import sys
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.info()['X-Request-Id']
-        print(x_request_id)
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.info()['X-Request-Id']
+    print(x_request_id)
