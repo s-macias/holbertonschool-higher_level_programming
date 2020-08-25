840e29125cdee50cc11a34a677956fb30d4e369c
s-macias <1552@holbertonschool.com>
2020-08-25 03:20:28 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("\t- type: <class 'bytes'>")
-    print("\t- content: b'OK'")
-    print("\t- utf8 content: OK")
+    print("\t- type: {}".format(html))
+    print("\t- content: {}".format(type(html)))
+    print("\t- utf8 content: {}".format(html.decode("UTF-8")))
