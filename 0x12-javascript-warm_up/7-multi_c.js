5d3a406044ef70b74bdeed6baa1381e2298781b3
s-macias <1552@holbertonschool.com>
2020-09-16 05:08:32 +0000

    fixing indentation task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-    while (i < x) {
-     console.log('C is fun');
-     i++;
-     }
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
