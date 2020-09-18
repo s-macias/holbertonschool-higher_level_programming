988cf1292e8021b9c86cae77e1ef21b4800fb9d9
s-macias <1552@holbertonschool.com>
2020-09-18 23:42:42 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  console.log('Argument found');
+  console.log('Arguments found');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
